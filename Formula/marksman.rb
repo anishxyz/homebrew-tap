8348ee60b2470f34e7d83850c9bef88c00f6208c
anishagrawal <anishagrawal2003@gmail.com>
2024-07-14 03:44:35 -0400

    added marksmanx

--- Formula/marksman.rb
+++ Formula/marksman.rb
@@ -1,11 +1,11 @@
-class marksmanx < Formula
+class Marksman < Formula
   desc "Rust CLI to snipe reservations on Resy"
   homepage "https://github.com/anishxyz/marksman"
   url "https://github.com/anishxyz/marksman/releases/download/v0.2.0/marksman-0.2.0-x86_64-apple-darwin.tar.gz"
-  sha256 "8b45cffdfe91f52f67f420bfc616870387c20a57"
+  sha256 "8806c840229a6e6df813bd0a5ad8621a4294fe04b06f50cfb0294f180a7599ca"
   version "0.2.0"
 
   def install
-    bin.install "marksmanx"
+    bin.install "marksman"
   end
 end
